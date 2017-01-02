3019db4a980b645294833a3670bc10916ee6798c
Melanie Burger <melanie.brgr@gmail.com>
2017-01-02 15:32:16 -0500

    Include PropTypes

--- app/components/app.jsx
+++ app/components/app.jsx
@@ -1,6 +1,6 @@
 // app.jsx
 
-import React from 'react';
+import React, { PropTypes } from 'react';
 
 class App extends React.Component {
   constructor() {
@@ -9,13 +9,23 @@ class App extends React.Component {
       display: true
     };
   }
+  toggleDisplay() {
+    this.setState({ display: !this.state.display });
+  }
   render() {
-    let h1 = "Default React Starter App Header";
-    if (this.state.display) h1 = this.props.title;
+    let title = "My Default React Starter App";
+    if (this.state.display) title = this.props.title;
     return (
-      <h1>{h1}</h1>
+      <div>
+        <h1>{title}</h1>
+        <button onClick={this.toggleDisplay.bind(this)}>Toggle header text</button>
+      </div>
     );
   }
+};
+
+App.propTypes = {
+  title: PropTypes.string.isRequired
 };
 
 export default App;
